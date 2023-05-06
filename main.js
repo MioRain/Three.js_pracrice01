86662c7d490069da0a0385d1e89899b86a99e81b
MioRain <ericzero1593@hotmail.com>
2023-05-06 21:01:52 +0800

    feat: create createPumpkin function

--- main.js
+++ main.js
@@ -40,6 +40,38 @@ function createBlock(x, y, color) {
   scene.add(block)
 }
 
+function createPumpkin() {
+  const colorY = 0xff8000
+  const colorB = 0x000000
+  const lines = {
+    1: [1, 2, 3, 4, 12, 13, 14, 15],
+    2: [1, 2, 3, 13, 14, 15],
+    3: [1, 2, 6, 7, 9, 10, 14, 15],
+    4: [1, 5, 6, 7, 8, 9, 10, 11, 15],
+    5: [4, 5, 8, 11, 12],
+    6: [],
+    7: [],
+    8: [4, 5, 6, 7, 9, 10, 11, 12],
+    9: [4, 5, 6, 10, 11, 12],
+    10: [4, 5, 11, 12],
+    11: [1, 4, 12, 15],
+    12: [1, 2, 14, 15],
+    13: [1, 2, 3, 13, 14, 15],
+    14: [1, 2, 3, 4, 12, 13, 14, 15],
+    15: [1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15]
+  }
+
+  for (let y = 1; y <= 15; y++) {
+    for (let x = 1; x <= 15; x++) {
+      if (lines[y].includes(x)) {
+        createBlock(x - 8, y - 8, colorB)
+      } else {
+        createBlock(x - 8, y - 8, colorY)
+      }
+    }
+  }
+}
+
 init()
 render()
-createBlock(0, 0, 0xffffff)
+createPumpkin()
